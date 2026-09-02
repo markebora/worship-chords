b5374459dbfdadb78582c182b7c7960576187357
markebora <markebora65@gmail.com>
2026-09-02 18:38:00 +0800

    Delete vapid-public-key.js

--- vapid-public-key.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res){
-
-  res.setHeader('Access-Control-Allow-Origin', '*');
-
-  return res.status(200).json({
-    publicKey:process.env.VAPID_PUBLIC_KEY || ''
-  });
-
-}
